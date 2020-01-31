09324ac84bd61b36c63a0b718799e7652ea646e5
fox-sen <xr223r@gmail.com>
2020-02-01 01:47:35 +0700

    Create custom config for Pygal

--- python_repos.py
+++ python_repos.py
@@ -28,11 +28,26 @@ for repo_dict in repo_dicts:
 
 # Make visualization.
 my_style = LS('#333366', base_style=LCS)
-chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+
+# Refine the chart.
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 16
+my_config.label_font_size = 12
+my_config.major_label_font_size = 10
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+
+chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('normal', stars)
+
+
 chart.render_to_file('python_repos.svg')
 chart.render_to_png('python_repos.png')
 
